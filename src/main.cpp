2b87ee72fbad57e83dae9f146d84bf39f7d18cee
DGashu <rachmiel2000@hotmail.com>
2026-01-30 15:19:16 -0600

    Fixed create shader program problems and fragment shader source

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
 void processInput(GLFWwindow *);
-void shaderCreation();
+void shaderCreation(unsigned int &shaderProgram);
 
 const char *vertexShaderSource = "#version 330\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -14,7 +14,7 @@ const char *vertexShaderSource = "#version 330\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
-    "layour (location = 0) in vec3 aPos;\n"
+    "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
@@ -52,7 +52,27 @@ int main()
     }
     
     //Compiling shaders and creating shader program
-    shaderCreation();
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    shaderCreation(shaderProgram);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
 
     //Running the window
@@ -62,11 +82,19 @@ int main()
          
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
     return 0;
 }
@@ -81,7 +109,7 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
-void shaderCreation()
+void shaderCreation(unsigned int &shaderProgram)
 {
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader,1, &vertexShaderSource, NULL);
@@ -103,8 +131,10 @@ void shaderCreation()
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
-    unsigned int shaderProgram;
+    
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
